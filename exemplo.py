9959ece5b9597b196c5f06aeff0203295421bb2c
YaraDeOliveira <79177224+YaraDeOliveira@users.noreply.github.com>
2021-03-15 22:02:20 -0300

    ajuste no exemplo

--- exemplo.py
+++ exemplo.py
@@ -1,4 +1,4 @@
 numero1 = int(input('Digite um numero: '))
 numero2 = int(input('Digite outro numero '))
 s = int((numero1 + numero2))
-print('A soma vale {}'.format(s))
+print('A soma dos numeros vale {}'.format(s))

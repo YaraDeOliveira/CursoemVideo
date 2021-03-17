fd1c16257140e15ab3ae076ad80bd6d9799dbd15
YaraDeOliveira <79177224+YaraDeOliveira@users.noreply.github.com>
2021-03-17 18:57:07 -0300

    Add files via upload
    Listas Compostas

---
+++ ex086.py
@@ -0,0 +1,7 @@
+matriz = list()
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz.append(int(input(f'Digite um valor para [{l} , {c}]:')))
+print(f'[ {matriz[0]} ] [ {matriz[1]} ] [ {matriz[2]} ]')
+print(f'[ {matriz[3]} ] [ {matriz[4]} ] [ {matriz[5]} ]')
+print(f'[ {matriz[6]} ] [ {matriz[7]} ] [ {matriz[8]} ]')

e0770846eb92c6eee41827decb2a41ceee9cfbef
YaraDeOliveira <79177224+YaraDeOliveira@users.noreply.github.com>
2021-03-24 14:22:09 -0300

    Add files via upload

---
+++ ex097.py
@@ -0,0 +1,9 @@
+def escreva(msg):
+    print(f'-'*(len(msg)+4))
+    print(f'  {msg:}')
+    print(f'-'*(len(msg)+4))
+
+
+escreva('Yara Regina')
+escreva('Curso de Python')
+escreva('Vou conseguir um estagio logo')

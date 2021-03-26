e5ce5158aad9cfc16150d6cb743d76da8acbee95
YaraDeOliveira <79177224+YaraDeOliveira@users.noreply.github.com>
2021-03-26 15:26:37 -0300

    Add files via upload

---
+++ moeda.py
@@ -0,0 +1,18 @@
+def aumentar(a, b):
+    s = a + (a * (b/100))
+    return s
+
+
+def diminuir(c, d):
+    m = c - (c * (d / 100))
+    return m
+
+
+def dobro(a):
+    d = a * 2
+    return d
+
+
+def metade(a):
+    d = a / 2
+    return d
